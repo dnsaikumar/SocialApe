627e09e9e807d2869c26f29ad966fafc3fbd449c
Nagendra Sai Kumar Dasari <nagendrasai.d@imimobile.com>
2019-09-05 12:05:15 +0530

    Refactored, and Simplified

---
+++ socialape-functions/functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports =  {
+    apiKey: "AIzaSyDKA_9aMPjHaLKjnrwgyXltaYaXZxoStV8",
+    authDomain: "socialape-192e3.firebaseapp.com",
+    databaseURL: "https://socialape-192e3.firebaseio.com",
+    projectId: "socialape-192e3",
+    storageBucket: "socialape-192e3.appspot.com",
+    messagingSenderId: "1075884984514",
+    appId: "1:1075884984514:web:98da7f3135cf92fa"
+};
